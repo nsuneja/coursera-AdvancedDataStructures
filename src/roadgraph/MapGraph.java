5afc69a862d7e92761a8c9c2865a609a7307c074
Nishant Suneja <nishant.suneja@maxta.com>
2016-09-07 18:04:56 -0700

    Course Project Extension

--- src/roadgraph/MapGraph.java
+++ src/roadgraph/MapGraph.java
@@ -8,12 +8,13 @@
 package roadgraph;
 
 
-import java.net.Authenticator;
+import java.nio.file.Path;
 import java.util.AbstractMap;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.LinkedHashSet;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
@@ -110,21 +111,26 @@ class MapEdge
  * a)  List of all the outgoing edges from this vertex.
  * b)  The geographical location of the vertex.
  * c)  The geographical location of the parent of the vertex discovered as
- *     we traverse the graph.  
+ *     we traverse the graph.
  * 
  * @author nishantsuneja
  *
  */
 class MapVertex
 {
-    private GeographicPoint location;
-    private GeographicPoint parentLocation;
+    private final GeographicPoint location;
+    // This map stores the parent location of this vertex, while
+    // traveling to a given destination vertex. The destination vertex
+    // is the key and the value is the parent location of
+    // this vertex, with respect to the given destination.
+    private Map<GeographicPoint, GeographicPoint> parentLocationMap;
     private List<MapEdge> outEdges;
     private double distanceFromOrigin;
 
     public MapVertex(GeographicPoint location) {
         this.location = location;
-        this.parentLocation = null;
+        this.parentLocationMap =
+                new HashMap<GeographicPoint, GeographicPoint>();
         this.outEdges = new ArrayList<MapEdge>();
         this.distanceFromOrigin = Double.MAX_VALUE;
     }
@@ -132,10 +138,6 @@ class MapVertex
     public GeographicPoint getLocation()
     {
         return location;
-    }
-    public void setLocation(GeographicPoint location)
-    {
-        this.location = location;
     }
     public List<MapEdge> getOutEdges()
     {
@@ -146,14 +148,36 @@ class MapVertex
         this.outEdges = outEdges;
     }
 
-    public GeographicPoint getParentLocation()
+    /**
+     * This vertex can have multiple parents based upon the end location
+     * of the search. This method retrieves the parent location for the
+     * given end location.
+     * @param endLocation
+     * @return
+     */
+    public GeographicPoint getParentLocation(GeographicPoint endLocation)
     {
-        return parentLocation;
+        // The parent location of a vertex is now with respect to the
+        // end vertex location.
+        // NOTE: We use this method only after finding a path from source
+        // to destination. At this point each node's (in the path) parent
+        // should have been set.
+        assert(parentLocationMap.containsKey(endLocation));
+        return parentLocationMap.get(endLocation);
     }
 
-    public void setParentLocation(GeographicPoint parentLocation)
+    /**
+     * This vertex can have multiple parents based upon the end location
+     * of the search. This method sets the parent location for the
+     * given end location.
+     * @param parentLocation
+     * @param endLocation
+     */
+    public void setParentLocation(GeographicPoint parentLocation,
+                                  GeographicPoint endLocation)
     {
-        this.parentLocation = parentLocation;
+        assert((parentLocation != null) && (endLocation != null));
+        parentLocationMap.put(endLocation, parentLocation);
     }
 
     public double getDistanceFromOrigin()
@@ -209,13 +233,91 @@ class MapVertex
     }
 
     @Override
+    public int hashCode()
+    {
+        return new Double(41*location.getX() + 43*location.getY()).intValue();
+    }
+
+    @Override
     public boolean equals(Object obj)
     {
         MapVertex vertex = (MapVertex)obj;
-        return location.equals(vertex.getLocation());
+        double x1 = location.getX();
+        double y1 = location.getY();
+        double x2 = vertex.location.getX();
+        double y2 = vertex.location.getY();
+        boolean ret = (Double.compare(x1, x2) == 0) &&
+                      (Double.compare(y1, y2) == 0);
+        return ret;
     }
 
 }
+
+/**
+ * This class represents a tuple of any vertex in the graph, and given
+ * end vertex. We use this tuple as a key during graph search to
+ * determine if we already have an optimal path from the current
+ * vertex to the end vertex.
+ * @author nishantsuneja
+ *
+ */
+class PathLookupTuple {
+    public PathLookupTuple(GeographicPoint curLocation,
+                           GeographicPoint endLocation)
+    {
+        this.curLocation = curLocation;
+        this.endLocation = endLocation;
+    }
+
+    public GeographicPoint getCurLocation()
+    {
+        return curLocation;
+    }
+
+    public GeographicPoint getEndLocation()
+    {
+        return endLocation;
+    }
+
+    @Override
+    public int hashCode()
+    {
+        return new Double(23*curLocation.getX() + 29*curLocation.getY() +
+               31*endLocation.getX() + 37*endLocation.getY()).intValue();
+    }
+
+    @Override
+    public boolean equals(Object obj)
+    {
+        PathLookupTuple tuple = (PathLookupTuple)obj;
+        double x1  = tuple.curLocation.getX();
+        double y1 = tuple.curLocation.getY();
+        double x2 = tuple.endLocation.getX();
+        double y2 = tuple.endLocation.getY();
+
+        double x3 = curLocation.getX();
+        double y3 = curLocation.getY();
+        double x4 = endLocation.getX();
+        double y4 = endLocation.getY();
+
+        boolean ret = (Double.compare(x1, x3) == 0) &&
+                       (Double.compare(y1, y3) == 0) &&
+                       (Double.compare(x2, x4) == 0) &&
+                       (Double.compare(y2, y4) == 0);
+        return ret;
+    }
+
+    @Override
+    public String toString()
+    {
+        return "PathLookupTuple [curLocation=" + curLocation + ", endLocation="
+                + endLocation + "]";
+    }
+
+    private final GeographicPoint curLocation;
+    private final GeographicPoint endLocation;
+}
+
 
 /**
  * This class represents a graph of geographic locations.
@@ -227,6 +329,12 @@ class MapVertex
 public class MapGraph {
 
     private Map<GeographicPoint, MapVertex> vertexMap;
+    // This set caches all the existing SHORTEST paths from a given
+    // location to another given location in the map. We use this set
+    // to optimizes the search algorithms, as we avoid running the
+    // algorithm on the "common" part of the different searches, performed
+    // on the same graph instance.
+    private Set<PathLookupTuple> shortestPathExists;
     private int numVertices;
     private int numEdges;
     private enum SearchAlgo {DIJKSTRA, ASTAR};
@@ -235,14 +343,14 @@ public class MapGraph {
      * This method resets the map graph to its earlier pristine state
      * before running a new search algorithm.
      */
-    private void resetMap() {
+    private void initializeGraph() {
+        // Set the distances to inifinity.
         for (GeographicPoint location: vertexMap.keySet()) {
             MapVertex vertex = vertexMap.get(location);
             vertex.setDistanceFromOrigin(Double.MAX_VALUE);
-            vertex.setParentLocation(null);
         }
     }
-  
+
     /**
      * This method validates the feasibility of adding an edge to the graph.
      * @return
@@ -280,6 +388,29 @@ public class MapGraph {
         return true;
     }
 
+
+    /**
+     * This method returns the path between start and end vertices.
+     * @param startVertex
+     * @param endVertex
+     * @param path
+     */
+    private void calculatePath(MapVertex startVertex,
+                               MapVertex endVertex,
+                               List<GeographicPoint> path)
+    {
+        MapVertex vertex;
+
+        // Calculate the path.
+        vertex = endVertex;
+        while (vertex != null) {
+            path.add(0, vertex.getLocation()); // Append to the beginning of the list.
+            vertex = vertexMap.get(vertex.getParentLocation
+                                   (endVertex.getLocation()));
+        }
+    }
+ 
+
     /** Find the path from start to goal using Dijkstra's algorithm/A* algorithm.
      * 
      * @param start The starting location
@@ -305,11 +436,14 @@ public class MapGraph {
         MapVertex endVertex;
         MapVertex vertex; 
         double newDistanceFromOrigin;
+        boolean pathFound;
         // Foe debugging only
         int numVerticesVisisted;
 
-        // Lets first reset the map graph, before running the search.
-        resetMap();
+        // Lets first reset the map graph, before running the search,
+        // because the user might run multiple searches on the same
+        // graph instance.
+        initializeGraph();
 
         // Ensure that the start and end points are present in the graph.
         if (!vertexMap.containsKey(start)) {
@@ -333,19 +467,43 @@ public class MapGraph {
          * the vertex which is closest to the source.
          */
         numVerticesVisisted = 0;
+        pathFound = false;
         while (!queue.isEmpty()) {
             vertex = queue.remove();
+
+            // For graph search visualization.
             nodeSearched.accept(vertex.getLocation());
+
             // For debugging only.
             numVerticesVisisted++;
-            System.out.println(searchAlgo.toString() + 
-               " VISITING [NODE at location ("+ vertex.getLocation()+")]");
-            System.out.println("Visited set size::" + visitedSet.size());
+
+            //System.out.println(searchAlgo.toString() + 
+            //   " VISITING [NODE at location ("+ vertex.getLocation()+")]");
+
             if (!visitedSet.contains(vertex.getLocation())) {
                 visitedSet.add(vertex.getLocation()); // Mark the vertex as visited.
 
+                // We found the shortest path from start to this vertex.
+                PathLookupTuple tuple1 = new PathLookupTuple(startVertex.getLocation(),
+                                                            vertex.getLocation());
+                shortestPathExists.add(tuple1);
+
+                // Check if we already have a cached path from the current
+                // vertex to the goal.
+                PathLookupTuple tuple2 = new PathLookupTuple(vertex.getLocation(),
+                                                    endVertex.getLocation());
+                if (shortestPathExists.contains(tuple2)) {
+                    pathFound = true;
+                    System.out.println("Path from::" + vertex + " to " +
+                            endVertex + " already cached. Terminating the search...");
+                    break;
+                }
+
                 // Check if we reached our goal.
-                if (vertex.getLocation().equals(goal)) {
+                if (vertex.getLocation().equals(endVertex.getLocation())) {
+                    System.out.println("Found the vertex::" + endVertex +
+                                       ". Terminating the search...");
+                    pathFound = true;
                     break;
                 }
 
@@ -375,7 +533,8 @@ public class MapGraph {
 
                         // Update the parent for the neighbor, as we found
                         // a new route.
-                        neighbor.setParentLocation(vertex.getLocation());
+                        neighbor.setParentLocation(vertex.getLocation(),
+                                                   endVertex.getLocation());
 
                         // Add the vertex's neighbor to the queue. 
                         queue.add(neighbor);
@@ -385,21 +544,14 @@ public class MapGraph {
         }
 
         // If no path is found, return null.
-        if (!vertex.equals(endVertex)) { // Path not found.
-            System.out.println("Path not found...");
+        if (!pathFound) { // Path not found.
+            System.out.println("No path found from::" + startVertex +
+                    " to " + endVertex);
             return null;
         }
 
-        // Calculate the path.
-        assert(vertex.equals(endVertex)); // Safety check.
-        while (vertex.getParentLocation() != null) {
-            path.add(0, vertex.getLocation()); // Append to the beginning of the list.
-            vertex = vertexMap.get(vertex.getParentLocation());
-        }
-
-        // We reached the start point.
-        assert(vertex.equals(startVertex));
-        path.add(0, startVertex.getLocation());
+        // Calculate the path
+        calculatePath(startVertex, endVertex, path);
 
         return new AbstractMap.SimpleEntry<List<GeographicPoint>, Integer>
                                                (path, numVerticesVisisted);
@@ -464,12 +616,14 @@ public class MapGraph {
         }
     }
 
+
 	/** 
 	 * Create a new empty MapGraph 
 	 */
 	public MapGraph()
 	{
 		vertexMap = new HashMap<GeographicPoint, MapVertex>();
+	    shortestPathExists = new LinkedHashSet<PathLookupTuple>();
 		numVertices = 0;
 		numEdges = 0;
 	}
@@ -584,17 +738,21 @@ public class MapGraph {
 	 *   path from start to goal (including both start and goal).
 	 */
 	public List<GeographicPoint> bfs(GeographicPoint start, 
-			 					     GeographicPoint goal, Consumer<GeographicPoint> nodeSearched)
+			 					     GeographicPoint goal,
+			 					     Consumer<GeographicPoint> nodeSearched)
 	{
 	    Queue<MapVertex> bfsQueue = new LinkedList<MapVertex>();
 	    Set<GeographicPoint> visitedSet = new HashSet<GeographicPoint>();
 	    List<GeographicPoint> path = new LinkedList<GeographicPoint>();
 	    MapVertex startVertex;
 	    MapVertex endVertex;
-	    MapVertex vertex; 
+	    MapVertex vertex;
+	    boolean pathFound;
 
-        // Lets first reset the map graph, before running the search.
-        resetMap();
+	    // Lets first reset the map graph, before running the search,
+        // because the user might run multiple searches on the same
+        // graph instance.
+	    initializeGraph();
 
 	    // Ensure that the start and end points are present in the graph.
 	    if (!vertexMap.containsKey(start)) {
@@ -615,13 +773,25 @@ public class MapGraph {
 	     * Move breath wise through the graph till we encounter the
 	     * goal vertex.
 	     */
+	    pathFound = false;
 	    while (!bfsQueue.isEmpty()) {
 	        vertex = bfsQueue.remove();
+	        // For visualization of the graph traversal
 	        nodeSearched.accept(vertex.getLocation());
             visitedSet.add(vertex.getLocation()); // Mark the vertex as visited.
+ 
+            // We found the shortest path from start to this vertex.
+            shortestPathExists.add(new PathLookupTuple(startVertex.getLocation(),
+                                               vertex.getLocation()));
 
 	        // Check if we reached our goal.
-	        if (vertex.getLocation().equals(goal)) {
+	        if (vertex.getLocation().equals(endVertex.getLocation()) ||
+                    // We already have a cached path from the current
+                    // vertex to the goal.
+                    shortestPathExists.contains(new PathLookupTuple
+                                        (vertex.getLocation(),
+                                        endVertex.getLocation()))) {
+	            pathFound = true;
 	            break;
 	        }
 
@@ -639,26 +809,21 @@ public class MapGraph {
 	                // Enqueue the newly discovered vertex.
 	                bfsQueue.add(neighbor);
 	                // Maintain the parent child relationship
-	                neighbor.setParentLocation(vertex.getLocation());
+	                neighbor.setParentLocation(vertex.getLocation(),
+	                                           endVertex.getLocation());
 	            }
 	        }
 	    }
 
 	    // If no path is found, return null.
-	    if (!vertex.equals(endVertex)) { // Path not found.
+	    if (!pathFound) { // Path not found.
+	        System.out.println("No path found from::" + startVertex +
+	                           " to " + endVertex);
 	        return null;
 	    }
 
-	    // Calculate the path.
-	    assert(vertex.equals(endVertex)); // Safety check.
-	    while (vertex.getParentLocation() != null) {
-	        path.add(0, vertex.getLocation()); // Append to the beginning of the list.
-	        vertex = vertexMap.get(vertex.getParentLocation());
-	    }
-
-	    // We reached the start point.
-	    assert(vertex.equals(startVertex));
-	    path.add(0, startVertex.getLocation());
+	    // Calculate the path from start to end.
+	    calculatePath(startVertex, endVertex, path);
 
 		return path;
 	}
@@ -740,6 +905,28 @@ public class MapGraph {
 
 	public static void main(String[] args)
 	{
+	    /*Set<MapVertex> gSet = new HashSet<MapVertex>();
+	    Set<PathLookupTuple> tSet = new HashSet<PathLookupTuple>();
+        GeographicPoint g1 = new GeographicPoint(8.0, -1.0);
+        GeographicPoint g2 = new GeographicPoint(8.0, -1.0);
+
+        MapVertex v1 = new MapVertex(g1);
+        MapVertex v2 = new MapVertex(g2);
+
+        System.out.println(v1.hashCode() + "," + v2.hashCode());
+        System.out.println(v1.equals(v2));
+
+        gSet.add(v1);
+        gSet.add(v2);
+        System.out.println(gSet);
+
+        PathLookupTuple t1 = new PathLookupTuple(g1, g2);
+        PathLookupTuple t2 = new PathLookupTuple(g1, g2);
+
+        tSet.add(t1);
+        tSet.add(t2);
+        System.out.println(tSet);*/
+
 	    MapGraph simpleTestMap = new MapGraph();
         GraphLoader.loadRoadMap("data/testdata/simpletest.map", simpleTestMap);
 
@@ -749,6 +936,9 @@ public class MapGraph {
         System.out.println("Test 1 using simpletest: Dijkstra should be 9 and AStar should be 5");
         List<GeographicPoint> testroute = simpleTestMap.dijkstra(testStart,testEnd);
         List<GeographicPoint> testroute2 = simpleTestMap.aStarSearch(testStart,testEnd);
+        System.out.println(testroute);
+        System.out.println(testroute2);
+        assert(testroute.equals(testroute2));
 
         MapGraph testMap = new MapGraph();
         GraphLoader.loadRoadMap("data/maps/utc.map", testMap);
@@ -759,6 +949,8 @@ public class MapGraph {
         System.out.println("Test 2 using utc: Dijkstra should be 13 and AStar should be 5");
         testroute = testMap.dijkstra(testStart,testEnd);
         testroute2 = testMap.aStarSearch(testStart,testEnd);
+        System.out.println(testroute);
+        System.out.println(testroute2);
 
         // A slightly more complex test using real data
         testStart = new GeographicPoint(32.8674388, -117.2190213);
@@ -766,11 +958,9 @@ public class MapGraph {
         System.out.println("Test 3 using utc: Dijkstra should be 37 and AStar should be 10");
         testroute = testMap.dijkstra(testStart,testEnd);
         testroute2 = testMap.aStarSearch(testStart,testEnd);
+        System.out.println(testroute);
+        System.out.println(testroute2);
 
-        testStart = new GeographicPoint(32.8648772, -117.2254046);
-        testEnd = new GeographicPoint(32.8660691, -117.217393);
-        testroute = testMap.dijkstra(testStart,testEnd);
-        testroute2 = testMap.aStarSearch(testStart,testEnd);
 	}
 	
 }
